1f93fd8d36c23412ae5aaea736020569b1f92bb3
Vegsja <vgsjaavik@gmail.com>
2022-03-06 20:06:44 +0100

    Lol

--- tempmonitor.sh
+++ tempmonitor.sh
@@ -10,9 +10,11 @@ while true
 do
 	temp=$(vcgencmd measure_temp | egrep -o '[0-9]*\.[0-9]*')
 	temp_rounded=${temp%%.*} 
-	if [ $temp_rounded -gt 60 ]
+	if [ $temp_rounded -gt 70 ]
 	then
+		now=$(date)
 		echo "Temperature is over limit. Shutting of...."
+		echo "$now - Temperature $temp over limit 70. Shutting down"
 		sleep 1
 		shutdown now
 		exit 0
