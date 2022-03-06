4a8e11d8b92b95657df13d04d6288c9650fceecf
Vegard Sjåvik <vgsjaavik@gmail.com>
2022-03-06 20:23:37 +0100

    Moved elements to top right

--- config/config.js
+++ config/config.js
@@ -47,41 +47,18 @@ var config = {
 		},
 		{
 			module: "clock",
-			position: "top_left"
+			position: "top_right"
 		},
 		{
 			module: "calendar",
 			header: "US Holidays",
-			position: "top_left",
+			position: "top_right",
 			config: {
 				calendars: [
 					{
 						symbol: "calendar-check",
 						url: "webcal://www.calendarlabs.com/ical-calendar/ics/76/US_Holidays.ics"					}
 				]
-			}
-		},
-		{
-			module: "compliments",
-			position: "lower_third"
-		},
-		{
-			module: "currentweather",
-			position: "top_right",
-			config: {
-				location: "New York",
-				locationID: "5128581", //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
-				appid: "YOUR_OPENWEATHER_API_KEY"
-			}
-		},
-		{
-			module: "weatherforecast",
-			position: "top_right",
-			header: "Weather Forecast",
-			config: {
-				location: "New York",
-				locationID: "5128581", //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
-				appid: "YOUR_OPENWEATHER_API_KEY"
 			}
 		},
 		{
