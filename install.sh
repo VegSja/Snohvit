ee1309d14e54c4e1d70fc44dc8fff1661f8cc9eb
Vegard Sjåvik <vgsjaavik@gmail.com>
2022-07-02 17:45:37 +0200

    Updated installation script

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 
 echo "Installing dependencies for MagicMirror"
 sleep 1
-npm install --only=prod --omit=dev &
+npm install --only=prod --omit=dev &> /dev/null
 
 echo "Installing dependencies for MMM-Entur-tavle"
 sleep 1
 cd modules/MMM-Entur-tavle
-npm install &
+npm install &> /dev/null
 cd ../..
 
 echo "Installation complete! Run launch.sh to launch Magic Mirror on your Raspberry PI"
